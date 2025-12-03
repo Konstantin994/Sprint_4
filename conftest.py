1cb2eb002108290a42ae78410142a5c780d94d9d
Konstantin994 <smithjohn9562@gmail.com>
2025-12-03 23:23:41 +0500

    second_pull_request

--- conftest.py
+++ conftest.py
@@ -8,14 +8,3 @@ def books_collector():
 
     return books_collector
 
-@pytest.fixture
-def genre():
-    genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
-
-    return genre
-
-@pytest.fixture
-def genre_age_rating():
-    genre_age_rating = ['Ужасы', 'Детективы']
-
-    return genre_age_rating
--- test.py
+++ test.py
@@ -4,11 +4,13 @@ from main import BooksCollector
 
 class TestBooksCollector:
     
-    def test_genre_is_true(self,books_collector,genre):
-        assert books_collector.genre[0] == genre[0] and books_collector.genre[1] == genre[1] and books_collector.genre[2] == genre[2] and books_collector.genre[3] == genre[3] and books_collector.genre[4] == genre[4] 
+    def test_genre_is_true(self,books_collector):
+        genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        assert books_collector.genre == genre
     
-    def test_genre_age_rating_is_true(self,books_collector, genre_age_rating):
-        assert books_collector.genre_age_rating[0] == genre_age_rating[0] and books_collector.genre_age_rating[1] == genre_age_rating[1]
+    def test_genre_age_rating_is_true(self,books_collector):
+        genre_age_rating = ['Ужасы', 'Детективы']
+        assert books_collector.genre_age_rating == genre_age_rating
 
     @pytest.mark.parametrize('name', ['И',
                                       'Иа',
@@ -29,18 +31,52 @@ class TestBooksCollector:
     
     def test_get_books_genre(self, books_collector):
         books_collector.add_new_book("Дюна")
+        books_collector.add_new_book("Сияние")
+        books_collector.add_new_book("Шерлок Холмс")
+
         books_collector.set_book_genre("Дюна", "Фантастика")
-        assert books_collector.get_books_genre() == {"Дюна": "Фантастика"}
+        books_collector.set_book_genre("Сияние", "Ужасы")
+        books_collector.set_book_genre("Шерлок Холмс", "Детективы")
+
+        expected = {
+        "Дюна": "Фантастика",
+        "Сияние": "Ужасы",
+        "Шерлок Холмс": "Детективы"
+        }
+
+        assert books_collector.get_books_genre() == expected
     
     def test_get_books_with_specific_genre(self, books_collector):
         books_collector.add_new_book("Дюна")
+        books_collector.add_new_book("Сияние")
+        books_collector.add_new_book("Шерлок Холмс")
+
         books_collector.set_book_genre("Дюна", "Фантастика")
-        assert books_collector.get_books_with_specific_genre("Фантастика") == ["Дюна"]
+        books_collector.set_book_genre("Сияние", "Ужасы")
+        books_collector.set_book_genre("Шерлок Холмс", "Фантастика")
+
+        result = books_collector.get_books_with_specific_genre("Фантастика")
+
+        assert "Дюна" in result
+        assert "Шерлок Холмс" in result
+        assert "Сияние" not in result
 
     def test_get_books_genre(self, books_collector):
         books_collector.add_new_book("Дюна")
+        books_collector.add_new_book("Сияние")
+        books_collector.add_new_book("Шерлок Холмс")
+
         books_collector.set_book_genre("Дюна", "Фантастика")
-        assert books_collector.get_books_genre() == {"Дюна": "Фантастика"}
+        books_collector.set_book_genre("Сияние", "Ужасы")
+        books_collector.set_book_genre("Шерлок Холмс", "Детективы")
+
+        expected = {
+        "Дюна": "Фантастика",
+        "Сияние": "Ужасы",
+        "Шерлок Холмс": "Детективы"
+        }
+
+        assert books_collector.get_books_genre() == expected
     
     @pytest.mark.parametrize(('book', 'genre', 'book_false','genre_false'), [
                                             ["Дюна", "Фантастика","Сияние","Ужасы"],
